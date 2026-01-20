329ae106d749b70e2dc2757cc8dc397e8df509f0
Kendo <s6701012610376@email.kmutnb.ac.th>
2026-01-20 21:57:17 +0700

    Chapter 5 mid

--- lists/tests.py
+++ lists/tests.py
@@ -7,10 +7,17 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")  
+        self.assertTemplateUsed(response, "home.html")
 
-    def test_renders_homepage_content(self):
+    def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, "To-Do") 
+        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<input name="item_text"')  
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html") 
+
 
     
