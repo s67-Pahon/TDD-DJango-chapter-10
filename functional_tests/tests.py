dc97c5ad30add79e918eef62f487c91a81fd5cf8
Kendo <s6701012610376@email.kmutnb.ac.th>
2026-02-03 00:29:32 +0700

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,13 +8,13 @@ from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
-MAX_WAIT = 5 
+MAX_WAIT = 10 
 
 FFoptions=Options()
 FFservice=Service(executable_path="/snap/bin/geckodriver")
 
 
-class NewVisitorTest(LiveServerTestCase):  
+class NewVisitorTest(StaticLiveServerTestCase):  
     def setUp(self):  
         self.browser = webdriver.Firefox(options=FFoptions,service=FFservice)
 
