f98dd03dcc1694880008581495850db673ecc9b9
Kendo <s6701012610376@email.kmutnb.ac.th>
2026-02-17 11:40:00 +0700

    Split out unit tests into two files

---
+++ src/lists/tests/test_models.py
@@ -0,0 +1,36 @@
+import lxml.html
+from django.test import TestCase
+from django.http import HttpRequest  
+from lists.views import home_page
+from lists.models import Item, List
+
+
+
+
+class ListAndItemModelsTest(TestCase):
+     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.list = mylist
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.list = mylist
+        second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, mylist)
+        self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, mylist)
--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -64,34 +64,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, "other list item")
 
 
-class ListAndItemModelsTest(TestCase):
-     def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = mylist
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
-
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
